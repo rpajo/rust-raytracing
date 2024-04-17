54b595c2a9e4383f098bf1a61ed76f96b458e844
Rok Pajnic <rok.pajnic@outlook.com>
2024-04-17 22:25:36 +0200

    feat: add world abstraction

---
+++ src/world.rs
@@ -0,0 +1,36 @@
+use std::rc::Rc;
+
+use crate::{
+    objects::object::{HitRecord, Object},
+    ray::Ray,
+};
+
+pub struct World {
+    pub objects: Vec<Rc<dyn Object>>,
+}
+
+impl World {
+    pub fn new() -> Self {
+        World {
+            objects: Vec::new(),
+        }
+    }
+
+    pub fn add_object<T: 'static + Object>(&mut self, object: T) {
+        self.objects.push(Rc::new(object));
+    }
+
+    pub fn hit_objects(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut hit = None;
+        let mut nearest_hit = t_max;
+
+        for obj in &self.objects {
+            if let Some(h) = obj.hit(ray, t_min, nearest_hit) {
+                nearest_hit = h.ray_scalar;
+                hit = Some(h);
+            }
+        }
+
+        hit
+    }
+}
