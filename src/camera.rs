7ad147af65892b6d07a09bf737e993f549a55e39
Rok Pajnic <rok.pajnic@outlook.com>
2024-04-18 21:08:29 +0200

    feat: camera to own module

---
+++ src/camera.rs
@@ -0,0 +1,105 @@
+use std::{
+    fs::OpenOptions,
+    io::{Error, Write},
+};
+
+use crate::{image::color_to_ppm, ray::Ray, vec3::Vec3, world::World};
+
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub render_image_width: i32,
+
+    render_image_heigh: i32,
+    viewport_height: f64,
+    viewport_width: f64,
+    focal_length: f64,
+    position: Vec3,
+
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+    pixel_00_loc: Vec3,
+}
+
+impl Camera {
+    pub fn new(image_width: i32, aspect_ratio: f64) -> Self {
+        let image_height = (image_width as f64 / aspect_ratio) as i32;
+        let image_ratio = image_width as f64 / image_height as f64;
+
+        let focal_length = 1.0;
+        let vp_height = 2.0;
+        let vp_width = vp_height + image_ratio;
+
+        let camera_position = Vec3::ZERO;
+        // view port relative vectors (x along the width and y along the height of the viewport)
+        // u => x axis in our basic case
+        // v => y axis in our basic case
+        // todo: if camera is pointing somewhere else, these vectors must also change
+        let vp_u_dir = Vec3::new(vp_width, 0.0, 0.0);
+        let vp_v_dir = Vec3::new(0.0, -vp_height, 0.0);
+        let vp_origin_upper_left = camera_position
+            - (0.5 * vp_u_dir)
+            - (0.5 * vp_v_dir)
+            - Vec3::new(0.0, 0.0, focal_length);
+
+        let pixel_delta_u = vp_u_dir / image_width as f64;
+        let pixel_delta_v = vp_v_dir / image_height as f64;
+        let pixel_00_loc = vp_origin_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        Camera {
+            aspect_ratio,
+            render_image_width: image_width,
+            render_image_heigh: image_height,
+            viewport_height: vp_height,
+            viewport_width: vp_width,
+            focal_length,
+            position: camera_position,
+            pixel_00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
+    }
+
+    pub fn render(&self, world: &World, filename: &str) -> Result<(), Error> {
+        // todo: move file logic outside of the camera
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(filename)?;
+
+        let mut write_buffer = String::new();
+
+        write_buffer.push_str(
+            format!(
+                "P3\n{} {}\n255\n",
+                self.render_image_width, self.render_image_heigh
+            )
+            .as_str(),
+        );
+
+        for y in 0..self.render_image_heigh {
+            // eprintln!("Scan-lines processed: {}/{}", y, render_image_height);
+
+            for x in 0..self.render_image_width {
+                let pixel_pos = self.pixel_00_loc
+                    + y as f64 * self.pixel_delta_v
+                    + x as f64 * self.pixel_delta_u;
+
+                let ray = Ray::new(self.position, pixel_pos - self.position);
+
+                let pixel_color = ray.ray_color(world);
+                write_buffer.push_str(&color_to_ppm(&pixel_color));
+            }
+        }
+
+        let write_result = file.write_all(write_buffer.as_bytes());
+        if write_result.is_err() {
+            eprintln!("Failed to write to file");
+            eprintln!("{:?}", write_result.err());
+        }
+        println!("Written to: {}", filename);
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use crate::ray::Ray;
+use camera::Camera;
+use objects::sphere::Sphere;
+
 use crate::vec3::Vec3;
 use crate::world::World;
-use crate::{image::color_to_ppm, objects::sphere::Sphere};
-use std::{fs::OpenOptions, io::Write};
 
+mod camera;
 mod image;
 mod objects;
 mod ray;
@@ -13,65 +14,18 @@ mod world;
 
 fn main() -> std::io::Result<()> {
     let filename = "render.ppm";
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(filename)?;
-
-    let mut write_buffer = String::new();
-
-    let image_aspect_ratio = 16.0 / 9.0;
-    let render_image_width = 1024;
-    let render_image_height = (render_image_width as f64 / image_aspect_ratio) as i32;
-
-    let viewport_height = 2.0;
-    let image_ratio = render_image_width as f64 / render_image_height as f64;
-    let viewport_width = image_ratio * viewport_height; // use `image_ratio` for exact ratio
-    let focal_length = 1.0;
-
-    let camera_pos = Vec3::ZERO;
-
-    // view port relative vectors (x along the width and y along the height of the viewport)
-    // u => x axis in our basic case
-    // v => y axis in our basic case
-    // todo: if camera is pointing somewhere else, these vectors must also change
-    let vp_u_dir = Vec3::new(viewport_width, 0.0, 0.0);
-    let vp_v_dir = Vec3::new(0.0, -viewport_height, 0.0);
-    let vp_origin_upper_left =
-        camera_pos - (0.5 * vp_u_dir) - (0.5 * vp_v_dir) - Vec3::new(0.0, 0.0, focal_length);
-
-    let pixel_delta_u = vp_u_dir / render_image_width as f64;
-    let pixel_delta_v = vp_v_dir / render_image_height as f64;
-    let pixel_00_loc = vp_origin_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 1024;
+    let camera = Camera::new(image_width, aspect_ratio);
 
     let mut world = World::new();
     world.add_object(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5));
     world.add_object(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0));
 
-    // Append ppm header
-    write_buffer
-        .push_str(format!("P3\n{} {}\n255\n", render_image_width, render_image_height).as_str());
-
-    for y in 0..render_image_height {
-        // eprintln!("Scan-lines processed: {}/{}", y, render_image_height);
-
-        for x in 0..render_image_width {
-            let pixel_pos = pixel_00_loc + y as f64 * pixel_delta_v + x as f64 * pixel_delta_u;
-
-            let ray = Ray::new(camera_pos, pixel_pos - camera_pos);
-
-            let pixel_color = ray.ray_color(&world);
-            write_buffer.push_str(&color_to_ppm(&pixel_color));
-        }
+    match camera.render(&world, filename) {
+        Ok(_) => println!("Rendering finished"),
+        Err(_) => eprintln!("Failed to render image"),
     }
 
-    let write_result = file.write_all(write_buffer.as_bytes());
-    if write_result.is_err() {
-        eprintln!("Failed to write to file");
-        eprintln!("{:?}", write_result.err());
-    }
-    println!("Written to: {}", filename);
     Ok(())
 }
