d80b29b1736ab7943861280d1b061be1336f2964
Rok Pajnic <rok.pajnic@outlook.com>
2024-04-28 21:08:44 +0200

    chore: move some helpers

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-use crate::vec3::Vec3;
+use crate::vec3::{Color3, Vec3};
 
 pub fn degrees_to_radians(deg: f64) -> f64 {
     deg * std::f64::consts::PI / 180.0
@@ -31,10 +31,11 @@ pub fn random_in_unit_disk() -> Vec3 {
 }
 
 pub fn linear_to_gamma_color(value: f64) -> f64 {
-    if value == 0.0 {
-        return 0.0;
+    if value > 0.0 {
+        value.sqrt()
+    } else {
+        0.0
     }
-    value.sqrt()
 }
 
 pub fn reflect_vector(vec: &Vec3, normal: &Vec3) -> Vec3 {
@@ -55,4 +56,12 @@ pub fn reflectance(cosine: f64, refraction_index: f64) -> f64 {
     // https://en.wikipedia.org/wiki/Schlick%27s_approximation
     let r0 = ((1.0 - refraction_index) / (1.0 + refraction_index)).powi(2);
     r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+}
+
+pub fn color_to_ppm(color: &Color3) -> String {
+    // transform from linear to gamma (gamma = 2)
+    let r = (linear_to_gamma_color(color.x).min(0.999999) * 256.0) as i32;
+    let g = (linear_to_gamma_color(color.y).min(0.999999) * 256.0) as i32;
+    let b = (linear_to_gamma_color(color.z).min(0.999999) * 256.0) as i32;
+    format!("{} {} {}\n", r, g, b)
 }
